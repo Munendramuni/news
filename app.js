fe68c23d6832a072134e1feead107843d21a33da
Munendramuni <blmunendraog@gmail.com>
2022-12-10 12:45:20 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+const request=require("request");
+const https=require("https")
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+	res.sendFile(__dirname+"/index.html")
+});
+
+app.post("/",function(req,res){
+
+	const fName=req.body.firstName;
+	const lName=req.body.lastName;
+	const email=req.body.email;
+	
+
+	const data={
+		members:[
+			   {
+			   	email_address:email,
+			   	status:"subscribed",
+			   	merge_fields: {
+			   		FNAME: fName,
+			   		LNAME: lName
+			   	}
+			   }
+			    ]
+	};
+	const jsonData=JSON.stringify(data);
+	const url=urlkey;
+	const  options={
+         method:"POST",
+         auth:apikey
+	};
+
+	const request= https.request(url,options,function(response){
+
+		if (response.statusCode === 200){
+			res.send("Successfully Subscribed");
+		}else{
+			res.send("Oops! Something Went Wrong! ")
+		}
+		response.on("data",function(data){
+               console.log(JSON.parse(data))
+		});
+	});
+	request.write(jsonData);
+	request.end();
+});
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+	console.log("server running")
+});
+
+
+
+
+
+
